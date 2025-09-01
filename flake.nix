c3c01a954e48f5786ae3ce984958ba364d6f251a
Philip Mørch <mrphil2105@gmail.com>
2025-09-01 16:04:09 +0200

    Update .gitignore

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Game Programming 2025 at IT University of Copenhagen";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      vscode-js-debug,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.cmake
+          ];
+        };
+      }
+    );
+}
